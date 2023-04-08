2298e7afb87921c5acbbc2eb26ae252a885bfbd0
Damian Kieltyka <damian.kieltyka@icloud.com>
2023-04-08 22:11:53 +0200

    Initial Commit

---
+++ ToDoAppSwiftUI/ToDoAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppSwiftUIApp.swift
+//  ToDoAppSwiftUI
+//
+//  Created by Damian Kiełtyka on 08/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
