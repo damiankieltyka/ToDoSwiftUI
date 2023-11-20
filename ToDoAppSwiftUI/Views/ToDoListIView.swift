0eb750b6465bfdc9de20c0f4c8dcacd5b951adb8
Damian Kieltyka <damian.kieltyka@icloud.com>
2023-11-20 23:03:01 +0100

    item delete

--- ToDoAppSwiftUI/Views/ToDoListIView.swift
+++ ToDoAppSwiftUI/Views/ToDoListIView.swift
@@ -10,11 +10,12 @@ import SwiftUI
 
 struct ToDoListView: View {
     
-    @StateObject var viewModel = ToDoListViewViewModel()
+    @StateObject var viewModel: ToDoListViewViewModel
     @FirestoreQuery var items: [ToDoListItem]
     
     init(userId: String) {
         self._items = FirestoreQuery(collectionPath: "users/\(userId)/todos")
+        self._viewModel = StateObject(wrappedValue: ToDoListViewViewModel(userId: userId))
     }
     
     var body: some View {
@@ -26,7 +27,7 @@ struct ToDoListView: View {
                             Button("Delete") {
                                 viewModel.delete(id: item.id)
                             }
-                            .foregroundColor(Color.red)
+                            .tint(.red)
                         }
                 }
             }
