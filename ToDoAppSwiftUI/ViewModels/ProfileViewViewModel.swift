b10c273d1658442140166a908b895c8c0309af6a
Damian Kieltyka <damian.kieltyka@icloud.com>
2023-11-21 23:27:47 +0100

    mark item as done

--- ToDoAppSwiftUI/ViewModels/ProfileViewViewModel.swift
+++ ToDoAppSwiftUI/ViewModels/ProfileViewViewModel.swift
@@ -5,12 +5,26 @@
 //  Created by Damian Kiełtyka on 17/05/2023.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 class ProfileViewViewModel: ObservableObject {
     init() {}
     
     func toogleIsDone(item: ToDoListItem) {
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
         
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
     }
 }
